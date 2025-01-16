e208da7b11d03a8401379e3ca44d5e71060b6ce4
David Augusto <davidakhaddad@gmail.com>
2025-01-16 19:13:13 -0300

    chore: atualização do .gitignore para incluir novas regras de exclusão

--- backend/dist/usuarios/enums/roles.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UsuarioRole = void 0;
-var UsuarioRole;
-(function (UsuarioRole) {
-    UsuarioRole["ADMIN"] = "admin";
-    UsuarioRole["USER"] = "user";
-})(UsuarioRole || (exports.UsuarioRole = UsuarioRole = {}));
